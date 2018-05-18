4aeda059a7850df3101d7d28efd90fcfb52d4bac
leleqin <602568224@qq.com>
2018-05-18 21:12:34 +0800

    完成

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+function main(km,min) {
+
+    let sum = 6;
+    let minSum = min * 0.25;
+
+    if (km <= 2){
+        sum += minSum;
+    }else if (km > 2 && km < 8){
+        sum += (km - 2)*0.8+minSum;
+    }else {
+        sum += (km - 7)*(0.8+0.8*0.5)+(7-2)*0.8+minSum;
+    }
+
+    return Math.round(sum);
 };
+module.exports = main;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,25 @@
-const main = require('../main/main');
+let main = require('../main/main');
 
-describe('taxi fee', function () {
+describe('main', function () {
     // write your tests here...
+    let km = [1,5,9];
+    let min = 3;
+    let pay = [7,9,13];
+
+    it('两公里以内车费多少', function () {
+        let result = main(km[0],min);
+        expect(result).toEqual(pay[0]);
+    });
+
+    it('八公里以内车费多少', function () {
+        let result = main(km[1],min);
+        expect(result).toEqual(pay[1]);
+    });
+
+    it('八公里外车费多少', function () {
+        let result = main(km[2],min);
+        expect(result).toEqual(pay[2]);
+    });
+
+
 });
